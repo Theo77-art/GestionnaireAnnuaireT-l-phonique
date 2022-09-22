643014ec7c0b421d1dadb96c64e30c640cf3dd2e
Theo <theo.tatsuki@gmail.com>
2022-09-23 00:42:01 +0200

    avancée sur le html/css

--- README.txt
+++ README.txt
@@ -1,2 +1,11 @@
 ---------- PROJET GESTIONNAIRE D'ANNUAIRE TÉLÉPHONIQUE -------------
 
+
+Palette de Couleur 
+
+#DAF7A6 Blanc
+#FFC300 Jaune
+#FF5733 Orange
+#C70039 Rouge
+#900C3F Bordeaux
+#581845 violet
--- index.php
+++ index.php
@@ -8,8 +8,20 @@
     <title>Document</title>
 </head>
 <body>
-    <div class="menu">
-
+    <div id="formulaire">
+        <h1>Formulaire d'enresitrement</h1>
+       <form action="" method="post">
+        <fieldset>
+         <label for="nom">Votre nom</label>
+         <input type="text" name="nom" id="nom" required>
+         <label for="prenom">Votre prénom</label>
+         <input type="text" name="prenom" id="prenom" required>
+         <label for="numero">Votre numéro de téléphone</label>
+         <input type="tel" name="numero" id="numero" required>
+         <input type="submit" value="Enregistrer">  
+        </fieldset>
+       </form>
+        <a href="tableau.php">Afficher les numéros enregistrés</a>
     </div>
 </body>
 </html>
